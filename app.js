9bc9351eb6484c460466fa7fbffe33be7ea76ca8
Senad Djerlek <senajerlek@gmail.com>
2022-07-04 19:01:42 +0200

    javascript test

---
+++ app.js
@@ -0,0 +1,106 @@
+// 1. Write a program to find the power of a number in JavaScript.
+// You are getting number from user input and then console logging
+// with template litteral notation.
+
+const novafunc = () => {
+   let unos = prompt("Unesi broj");
+   unos = parseFloat(unos);
+   const squareRoot = unos * unos
+   console.log(`Kvadrat broja ${unos} , je ${squareRoot}`)
+};
+
+novafunc();
+
+
+
+
+
+
+
+
+
+
+
+ 
+// Corrent console log should be: The pow of 5 is 25
+ 
+//2. Shopping Cart.
+// There are array of objects with all products that someone bought.
+//In product object we got name,
+// price per unit, and amount of units. Your job is to loop throught array,
+// and make new array of objects
+// with two key-value pairs name and total price for that product.
+// After you did the job with new array, you need to sum all prices of
+// every product and return total sum value, and add a FIXED 320 shipping costs.
+// After all of that you should print
+// in console following statement: 'You got ${num of products} in cart, 
+//shipping cost is ${shipping cost}, your total is ${total}'
+ 
+const CART_DATA = [
+    {
+      id: 1,
+      name: "Hawai Shirt",
+      price: 30,
+      amount: 2,
+      categorty: "summer",
+      rating: 4.5,
+    },
+    {
+      id: 3,
+      name: "Adidas Slippers",
+      price: 35,
+      amount: 1,
+      categorty: "summer",
+      rating: 4.5,
+    },
+    {
+      id: 6,
+      name: "White T-Shirt",
+      price: 15,
+      amount: 4,
+      categorty: "summer",
+      rating: 4.7,
+    },
+    {
+      id: 4,
+      name: "Summer hat",
+      price: 7.5,
+      amount: 3,
+      categorty: "summer",
+      rating: 40,
+    },
+  ];
+
+  const drugiZadatak = (arr) => {
+    const len = arr.length;
+    const shippingCost = 1920;
+    const namePrice = arr.map((value) => {
+        return {
+            name: value.name, totalPrice: value.price * value.amount
+        }
+    });
+    const sum = namePrice.map((value) => value.totalPrice).reduce((prevValue, currValue) => prevValue + currValue, shippingCost);
+    // return sum;
+    return `You got ${len} in cart, shipping cost is ${shippingCost}, your total is ${sum}`
+};
+
+// console.log(secondTask(CART_DATA));
+   
+  //3. Fetch data with async javascript(fetch or async/await) from this API
+  //https://reqres.in/api/users, where you need to add QUERY param called page and its value should be 2
+  //After you successfully fetch data, map that array into array with only data of users
+
+
+const NOVI_URL = "https://reqres.in/api"
+
+const uzmiUzere = async () => {
+
+    const uzmiUsere = await fetch(`${NOVI_URL}/users?page=2`);
+
+    const noviUseri = uzmiUsere.json();
+
+    return noviUseri;
+}
+
+uzmiUzere().then((res) => console.log(res.data))
+
